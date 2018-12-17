9c0f569b38fa4160bf8c8c9096e1a782b8c9ef69
nicolaguglielmi <45848139+nicolaguglielmi@users.noreply.github.com>
2018-12-17 09:16:12 +0100

    Add files via upload
    -fixed timeout error on oauth2
    -rewrite function
    -code clean

--- logs-parser.py
+++ logs-parser.py
@@ -11,25 +11,35 @@ refresh=10
 #log filename
 logfile='/var/lib/3cxpbx/Instance1/Data/Logs/3CXDialer.log'
 #ghoogle sheet name, please type your :)
-gsheetname='Your gsheet name'
+gsheetname='Call Logs'
 
 
-# use creds to create a client to interact with the Google Drive API
-scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-client = gspread.authorize(creds)
-
-# Find a workbook by name and open the first sheet
-# Make sure you use the right name here.
-sheet = client.open(gsheetname).sheet1
-
-date=time_=caller=called=""
+#last call counter
 last_call=0
 
-def match_write(newline):
+def sheet_write(values='',update=True):
+    #call_num,date,time,caller,called
+    # use credentials in json to create a client to interact with the Google Sheet
+    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    client = gspread.authorize(creds)
+    # Find a workbook by name and open the first sheet
+    sheet = client.open(gsheetname).sheet1
+    
+    if not update:
+        #write to google sheet the new values
+        sheet.insert_row(values.split(';'),2)
+        print('Last update:',now_time.strftime("%d/%m/%Y %H:%M:%S"),values.split(';'))
+    
+    if update:
+       #write just the updated timestamp
+       sheet.update_acell('F1','Last update:'+now_time.strftime("%d/%m/%Y %H:%M:%S"))
+       print('Last update:'+now_time.strftime("%d/%m/%Y %H:%M:%S"))
+
+def match_call(newline):
     global last_call
     call_num=0
+    date=time=caller=called=""
     
     #this matches an extension in the format 2xx, if you have another exensions numbering, please modify
     #the following part of the regex according to your needs: DN=[2][0-9]{2}.+ and put inside the regex below
@@ -44,8 +54,7 @@ def match_write(newline):
         #insert the data in the second row, always. The first row is the header row
         if call_num>last_call:
             last_call=call_num
-            sheet.insert_row([call_num,str(date),str(time),str(caller),str(called)],2)
-            print(last_call,call_num,date,time,caller,called)
+            sheet_write(';'.join([str(call_num),str(date),str(time),str(caller),str(called)]),False)
 
 
 
@@ -57,9 +66,9 @@ while True:
 
     if not newline:
         now_time=datetime.datetime.now()
-        sheet.update_acell('F1','Last update:'+now_time.strftime("%d/%m/%Y %H:%M:%S"))
+        sheet_write()
         time.sleep(refresh)
         continue
-    match_write(newline)
+    match_call(newline)
 
 
