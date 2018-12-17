435cbb953d1260de45a90d704473bd0cbe8f4260
nicolaguglielmi <45848139+nicolaguglielmi@users.noreply.github.com>
2018-12-17 19:53:11 +0100

    Add files via upload
    -minor fix
    -code clean

--- logs-parser.py
+++ logs-parser.py
@@ -1,5 +1,5 @@
 #3cx call log parse and write to gsheet
-#V4
+#V 0.5
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import re
@@ -12,14 +12,11 @@ refresh=10
 logfile='/var/lib/3cxpbx/Instance1/Data/Logs/3CXDialer.log'
 #google sheet name, please type your :)
 gsheetname='Call Logs'
-
-
 #last call counter
 last_call=0
 
-def sheet_write(values='',update=True):
 
-    #call_num,date,time,caller,called
+def sheet_write(values='',update=False):
     # use credentials in json to create a client to interact with the Google Sheet
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
@@ -27,21 +24,21 @@ def sheet_write(values='',update=True):
     # Find a workbook by name and open the first sheet
     sheet = client.open(gsheetname).sheet1
     
-    if not update:
+    if update:
         #write to google sheet the new values
         sheet.insert_row(values.split(';'),2)
         print('Last update:',datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"),values.split(';'))
     
-    if update:
+    if not update:
        #write just the updated timestamp
        sheet.update_acell('F1','Last update:'+datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
        print('Last update:'+datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+
 
 def match_call(newline):
     global last_call
     call_num=0
     date=time=caller=called=""
-    
     #this matches an extension in the format 2xx, if you have another exensions numbering, please modify
     #the following part of the regex according to your needs: DN=[2][0-9]{2}.+ and put inside the regex below
     matches=re.findall(r'[1-9]{2}\/[1-9]{2}\/[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}.+\[\bDialing\].+DN=[2][0-9]{2}.+',newline)
@@ -55,13 +52,11 @@ def match_call(newline):
         #insert the data in the second row, always. The first row is the header row
         if call_num>last_call:
             last_call=call_num
-            sheet_write(';'.join([str(call_num),str(date),str(time),str(caller),str(called)]),False)
+            sheet_write(';'.join([str(call_num),str(date),str(time),str(caller),str(called)]),True)
 
-
-
+#open the logfile and wait for new lines
 logfile_handle=open(logfile,"r")
 logfile_handle.seek(0,2)
-
 while True:
     newline = logfile_handle.readline()
     if not newline:
@@ -70,5 +65,3 @@ while True:
         continue
     else:
         match_call(newline)
-
-
