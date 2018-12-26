b2f40455d9aba6fe72ff47fc231e55772b24d7c1
nicolaguglielmi <45848139+nicolaguglielmi@users.noreply.github.com>
2018-12-26 22:32:31 +0100

    Add files via upload
    minor bugfix

--- logs-parser.py
+++ logs-parser.py
@@ -1,5 +1,5 @@
 #3cx call log parse and write to gsheet
-#V 0.5
+#V 0.6
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import re
@@ -14,9 +14,11 @@ logfile='/var/lib/3cxpbx/Instance1/Data/Logs/3CXDialer.log'
 gsheetname='Call Logs'
 #last call counter
 last_call=0
-
+#last update timestamp, workaround to the 100 calls/100 sec sliding windows limitation
+last_update=datetime.datetime.now()
 
 def sheet_write(values='',update=False):
+    global last_update
     # use credentials in json to create a client to interact with the Google Sheet
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
@@ -24,35 +26,52 @@ def sheet_write(values='',update=False):
     # Find a workbook by name and open the first sheet
     sheet = client.open(gsheetname).sheet1
     
+    #workaround for the api usage quotas
+    while (datetime.datetime.now()-last_update).seconds <1:
+        time.sleep(0.2)
+
     if update:
         #write to google sheet the new values
         sheet.insert_row(values.split(';'),2)
         print('Last update:',datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"),values.split(';'))
+        last_update=datetime.datetime.now()
+        
     
     if not update:
        #write just the updated timestamp
        sheet.update_acell('F1','Last update:'+datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
        print('Last update:'+datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+       last_update=datetime.datetime.now()
 
 
 def match_call(newline):
     global last_call
     call_num=0
     date=time=caller=called=""
+    Sheet_Buffer=""
     #this matches an extension in the format 2xx, if you have another exensions numbering, please modify
     #the following part of the regex according to your needs: DN=[2][0-9]{2}.+ and put inside the regex below
     matches=re.findall(r'[1-9]{2}\/[1-9]{2}\/[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}.+\[\bDialing\].+DN=[2][0-9]{2}.+',newline)
     for line in matches:
-        call_num=int(re.search(r'Call\(([\d]+)\)',line).group(1))
-        date=re.search(r'[1-9]{2}\/[1-9]{2}\/[0-9]{2}',line).group(0)
-        date=date[-2:]+date[2:-2]+date[0:2]
-        time=re.search(r'[0-9]{2}:[0-9]{2}:[0-9]{2}',line).group(0)
-        caller=re.search(r'DN=([\d]+)',line).group(1)
-        called=re.search(r'EP=([\d]+)',line).group(1)
-        #insert the data in the second row, always. The first row is the header row
-        if call_num>last_call:
-            last_call=call_num
-            sheet_write(';'.join([str(call_num),str(date),str(time),str(caller),str(called)]),True)
+        Result_Callnum=re.search(r'Call\(([\d]+)\)',line)
+        Result_Date=re.search(r'[1-9]{2}\/[1-9]{2}\/[0-9]{2}',line)
+        Result_Time=re.search(r'[0-9]{2}:[0-9]{2}:[0-9]{2}',line)
+        Result_Caller=re.search(r'DN=([\d]+)',line)
+        Result_Called=re.search(r'EP=([\d]+)',line)
+
+        #check for each text match:
+        if Result_Callnum and Result_Date and Result_Time and Result_Caller and Result_Called:
+            call_num=int(Result_Callnum.group(1))
+            date=Result_Date.group(0)
+            date=date[-2:]+date[2:-2]+date[0:2]
+            time=Result_Time.group(0)
+            caller=Result_Caller.group(1)
+            called=Result_Called.group(1)
+
+            #insert the data in the second row, always. The first row is the header row
+            if call_num>last_call:
+                last_call=call_num
+                sheet_write(';'.join([str(call_num),str(date),str(time),str(caller),str(called)]),True)
 
 #open the logfile and wait for new lines
 logfile_handle=open(logfile,"r")
